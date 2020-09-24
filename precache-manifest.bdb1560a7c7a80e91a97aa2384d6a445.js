a3a5e325f461a087cd764fae070cb0677eb5a763
Trevor Luper <luper4@gmail.com>
2020-09-24 10:16:10 -0500

    Updates

---
+++ precache-manifest.bdb1560a7c7a80e91a97aa2384d6a445.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a31f3fdaf6cdc5c9cfa74fb45b979b2",
+    "url": "/cards/index.html"
+  },
+  {
+    "revision": "b2aac23acdb415a00d96",
+    "url": "/cards/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "516f2851aadfa2dc7f4a",
+    "url": "/cards/static/css/main.6e617345.chunk.css"
+  },
+  {
+    "revision": "b2aac23acdb415a00d96",
+    "url": "/cards/static/js/2.a7908b87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cards/static/js/2.a7908b87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "516f2851aadfa2dc7f4a",
+    "url": "/cards/static/js/main.441edaa1.chunk.js"
+  },
+  {
+    "revision": "8e9c10436e990c8ebaef",
+    "url": "/cards/static/js/runtime-main.3640371d.js"
+  }
+]);
